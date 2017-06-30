8a629635f92bddd4598813541645a5d3aca07fa0
Horbunov Mykhailo <miwagorbi@gmail.com>
2017-06-30 15:36:09 +0300

    Increased coverage

--- src/main/java/com/voidaspect/checkpro/AES128EncryptionService.java
+++ src/main/java/com/voidaspect/checkpro/AES128EncryptionService.java
@@ -2,7 +2,8 @@ package com.voidaspect.checkpro;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Base64;
 
@@ -11,24 +12,20 @@ import java.util.Base64;
  */
 public final class AES128EncryptionService {
 
-    private static final String CHARSET = "UTF8";
+    private static final Charset CHARSET = StandardCharsets.UTF_8;
 
     private static final String ENCRYPTION_TYPE = "AES";
 
     private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
 
-    private static final int CHARS_IN_KEY = 128 / 8;
+    private static final int BYTES_IN_KEY = 128 / 8;
 
     private final byte[] key;
 
     public AES128EncryptionService(String key) {
         final byte[] keyBytes;
-        try {
-            keyBytes = key.getBytes(CHARSET);
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
-        this.key = Arrays.copyOf(keyBytes, CHARS_IN_KEY);
+        keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        this.key = Arrays.copyOf(keyBytes, BYTES_IN_KEY);
     }
 
     public String encrypt(String plainText) throws Exception {
--- src/test/java/com/voidaspect/checkpro/FormatterServiceTest.java
+++ src/test/java/com/voidaspect/checkpro/FormatterServiceTest.java
@@ -2,6 +2,7 @@ package com.voidaspect.checkpro;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.quicktheories.quicktheories.core.Source;
 import org.quicktheories.quicktheories.impl.TheoryBuilder;
 
 import java.util.function.Function;
@@ -9,6 +10,7 @@ import java.util.function.Function;
 import static com.voidaspect.checkpro.FormatterService.*;
 import static org.junit.Assert.assertEquals;
 import static org.quicktheories.quicktheories.QuickTheory.qt;
+import static org.quicktheories.quicktheories.generators.SourceDSL.arbitrary;
 import static org.quicktheories.quicktheories.generators.SourceDSL.strings;
 
 /**
@@ -42,25 +44,53 @@ public class FormatterServiceTest {
 
     @Test
     public void caseMustKeepStringLength() throws Exception {
-        assertStringLengthInvariant(Case.UPPER);
-        assertStringLengthInvariant(Case.LOWER);
-        assertStringLengthInvariant(Case.NONE);
+        qt().withExamples(Case.values().length).forAll(arbitrary().enumValues(Case.class))
+                .checkAssert(FormatterServiceTest::assertStringLengthInvariant);
     }
 
     @Test
     public void spacingMustKeepStringLength() throws Exception {
-        assertStringLengthInvariant(Spacing.DASHES);
-        assertStringLengthInvariant(Spacing.SPACES);
-        assertStringLengthInvariant(Spacing.UNDERSCORES);
-        assertStringLengthInvariant(Spacing.NONE);
+        qt().withExamples(Spacing.values().length).forAll(arbitrary().enumValues(Spacing.class))
+                .checkAssert(FormatterServiceTest::assertStringLengthInvariant);
     }
 
     @Test
     public void trimmingMustBeIdempotent() throws Exception {
-        assertOperationIdempotent(Trim.ALL);
-        assertOperationIdempotent(Trim.INTERNAL);
-        assertOperationIdempotent(Trim.START_AND_END);
-        assertOperationIdempotent(Trim.NONE);
+        qt().withExamples(Trim.values().length).forAll(arbitrary().enumValues(Trim.class))
+                .checkAssert(FormatterServiceTest::assertOperationIdempotent);
+    }
+
+    @Test
+    public void formattingMustBeIdempotent() throws Exception {
+        String initial = " I am  unformatted-string";
+        assertEquals(initial, formatterService.formatText(initial));
+        assertEquals(initial, formatterService.formatText(initial, Function.identity()));
+        qt().withExamples(10000).forAll(stringValues().andAlwaysTheValues(initial),
+                arbitrary().enumValues(Case.class),
+                arbitrary().enumValues(Trim.class),
+                arbitrary().enumValues(Spacing.class))
+                .checkAssert((text, c, t, s) -> {
+                    assertEquals(formatterService.formatText(text, c, c, c, t, t, t, s, s, s),
+                            formatterService.formatText(text, c, t, s));
+
+                    assertEquals(formatterService.formatText(text, c, c),
+                            formatterService.formatText(text, c, Trim.NONE, Spacing.NONE));
+
+                    assertEquals(formatterService.formatText(text, t, t),
+                            formatterService.formatText(text, Case.NONE, t, Spacing.NONE));
+
+                    assertEquals(formatterService.formatText(text, s, s),
+                            formatterService.formatText(text, Case.NONE, Trim.NONE, s));
+
+                    assertEquals(formatterService.formatText(text, c, c, t, t),
+                            formatterService.formatText(text, c, t, Spacing.NONE));
+
+                    assertEquals(formatterService.formatText(text, c, c, s, s),
+                            formatterService.formatText(text, c, Trim.NONE, s));
+
+                    assertEquals(formatterService.formatText(text, t, t, s, s),
+                            formatterService.formatText(text, Case.NONE, t, s));
+                });
     }
 
     @Test
@@ -73,11 +103,35 @@ public class FormatterServiceTest {
                 .asWithPrecursor(s -> formatterService.formatText(s, Case.LOWER, Spacing.DASHES))
                 .check((initial, formatted) ->
                         formatted.equals(formatterService.formatText(initial, Case.LOWER, Trim.NONE, Spacing.DASHES)));
-        //noinspection unchecked
         stringTheory()
                 .asWithPrecursor(s -> formatterService.formatText(s))
                 .check((initial, formatted) ->
                         formatted.equals(formatterService.formatText(initial, Case.NONE, Trim.NONE, Spacing.NONE)));
+
+        qt().withExamples(10000).forAll(stringValues(),
+                arbitrary().enumValues(Case.class),
+                arbitrary().enumValues(Trim.class),
+                arbitrary().enumValues(Spacing.class))
+                .checkAssert((initial, c, t, s) -> {
+                    assertEquals(formatterService.formatText(initial, c),
+                            formatterService.formatText(initial, c, Trim.NONE, Spacing.NONE));
+
+                    assertEquals(formatterService.formatText(initial, t),
+                            formatterService.formatText(initial, Case.NONE, t, Spacing.NONE));
+
+                    assertEquals(formatterService.formatText(initial, s),
+                            formatterService.formatText(initial, Case.NONE, Trim.NONE, s));
+
+                    assertEquals(formatterService.formatText(initial, c, t),
+                            formatterService.formatText(initial, c, t, Spacing.NONE));
+
+                    assertEquals(formatterService.formatText(initial, c, s),
+                            formatterService.formatText(initial, c, Trim.NONE, s));
+
+                    assertEquals(formatterService.formatText(initial, t, s),
+                            formatterService.formatText(initial, Case.NONE, t, s));
+                });
+
     }
 
     private static void assertStringLengthInvariant(Function<String, String> function) {
@@ -93,10 +147,14 @@ public class FormatterServiceTest {
     }
 
     private static TheoryBuilder<String> stringTheory() {
-        return qt().withExamples(10000).forAll(strings()
+        return qt().withExamples(10000).forAll(stringValues());
+    }
+
+    private static Source<String> stringValues() {
+        return strings()
                 .basicLatinAlphabet()
                 .ofLengthBetween(1, 1000)
-                .andAlwaysTheValues(" aeudh diao  ehod-aodO ", "aDjDio   ie j", "a_a L"));
+                .andAlwaysTheValues(" aeudh diao  ehod-aodO ", "aDjDio   ie j", "a_a L");
     }
 
 }
